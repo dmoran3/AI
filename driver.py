d3b8bd98c02b4d5dea802437182a7d996f05eed8
dmoran3 <dan3.moran3@gmail.com>
2022-03-01 13:01:56 -0500

    Updated March 1 2020 - 1PM

--- driver.py
+++ driver.py
@@ -6,10 +6,10 @@
 # 
 # 
 
-import numpy as np
-
-#Define goal state
-goal_state = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+#import numpy as np
+import sys
+import time
+#from resource import *
 
 #define the edges
 top_row = [0,1,2]
@@ -17,6 +17,7 @@ bottom_row = [6,7,8]
 right_side = [2,5,8]
 left_side = [0,3,6]
 
+goal_state = [0,1,2,3,4,5,6,7,8]
 
 class Node:
     #CONSTRUCTOR FUNCTION
@@ -33,7 +34,6 @@ class Node:
     def generate_children(self):
         #create empty array to store next states
         next_states = []
-
         #find the 0 in the current state
         zero = self.state.index(0)
 
@@ -43,79 +43,132 @@ class Node:
         #UP
         if(zero not in top_row):
             #Swap the value above zero with zero using a new state
-            new_state = self.state.copy()
-            temp = new_state[zero-3]
-            new_state[zero-3] = 0
-            new_state[zero] = temp
+            new_state = list(self.state[:])
+            temp = zero-3
+            new_state[temp] = 0
+            new_state[zero] = self.state[temp]
             #UP as a move is now a node that self can choose
-            UP = Node(new_state, self, "up", self.depth + 1, 1, self.path_cost +1)
+            UP = Node(new_state, self, "up", self.depth + 1, 1, self.total_path_cost +1)
             next_states.append(UP)
 
         #DOWN
         if(zero not in bottom_row):
             #Swap the value below zero with zero using a new state
-            new_state = self.state.copy()
-            temp = new_state[zero+3]
-            new_state[zero+3] = 0
-            new_state[zero] = temp
+            new_state = list(self.state[:])
+            temp = zero+3
+            new_state[temp] = 0
+            new_state[zero] = self.state[temp]
             #DOWN as a move is now a node that self can choose
-            DOWN = Node(new_state, self, "down", self.depth + 1, 1, self.path_cost +1)
+            DOWN = Node(new_state, self, "down", self.depth + 1, 1, self.total_path_cost +1)
             next_states.append(DOWN)
 
         #LEFT
         if(zero not in left_side):
             #Swap the value next to zero with zero using a new state
-            new_state = self.state.copy()
-            temp = new_state[zero-1]
-            new_state[zero-1] = 0
-            new_state[zero] = temp
+            new_state = list(self.state[:])
+            temp = zero-1
+            new_state[temp] = 0
+            new_state[zero] = self.state[temp]
             #LEFT as a move is now a node that self can choose
-            LEFT = Node(new_state, self, "left", self.depth + 1, 1, self.path_cost +1)
+            LEFT = Node(new_state, self, "left", self.depth + 1, 1, self.total_path_cost +1)
             next_states.append(LEFT)
 
         #RIGHT
         if(zero not in right_side):
             #Swap the value next to zero with zero using a new state
-            new_state = self.state.copy()
-            temp = new_state[zero+1]
-            new_state[zero+1] = 0
-            new_state[zero] = temp
+            new_state = list(self.state[:])
+            temp = zero+1
+            new_state[temp] = 0
+            new_state[zero] = self.state[temp]
             #RIGHT as a move is now a node that self can choose
-            RIGHT = Node(new_state, self, "right", self.depth + 1, 1, self.path_cost +1)
+            RIGHT = Node(new_state, self, "right", self.depth + 1, 1, self.total_path_cost +1)
             next_states.append(RIGHT)
 
+        return next_states
+
     def ManhattanDistance(self):
-        total = 0
+        total_distance = 0
         for number in self.state:
             number_index = self.state.index(number)
             goal_state_index = goal_state.index(number)
 
-    def solver(self, method, starting_state, goal_state):
-        print("In SOLVER method", method)
-        visited_nodes = [starting_state]
-        #__init__(self, state, parent, action, depth, cost, total_path_cost)
-        queue = [Node[starting_state, None, None, 0, 0, 0]]
-        nodes_popped = 0
-        max_queue = 0
+            #y position (row) found by floor division of the index
+            #x position (column) found by taking mod 3 of the index
+            #manhattanDistance is abs(difference in x position) + abs(difference in y position)
+            distance = abs((goal_state_index % 3) - (number_index % 3)) + abs((goal_state_index \
+                 // 3) - (number_index // 3))
+            total_distance += distance
+        return distance
 
-        while(len(queue)>0):
-            #Update queue information
-            max_queue = max(len(queue), max_queue)
-            current_node = queue.pop()
-            nodes_popped +=1
+def print_statistics(self, start_time, end_time, path_to_goal, cost_of_path, nodes_expanded, \
+        search_depth, max_search_depth, max_ram_usage):
+    print("path_to_goal: ", path_to_goal)
+    print("cost_of_path: ", cost_of_path)
+    print("nodes_expanded:  ", nodes_expanded)
+    print("search_depth: ", search_depth)
+    print("max_search_depth: ", max_search_depth)
+    print("running_time: ", (end_time - start_time))
+    print("max_ram_usage: ", max_ram_usage)
 
-            if current_node.state == goal_state:
-                return "GOAL STATE FOUND"
-            next_states = current_node.generate_children()
-            for node in next_states:
-                if node.state not in visited_nodes:
-                    visited_nodes.append(node.state)
-                    if method == "bfs":
-                        queue.insert(0, node )
-                        #Adding to the front of a queue gives FIFO Behavior
-                    else:
-                        queue.append(node)
-                        #Adding to the back of a queue gives LIFO Behavior
-            if method == "ast":
-                queue.sort(key = lambda node: node ManhattanDistance() + node.path_cost, reverse = True)
+def solver(method, starting_state, goal_state):
+    start_time = time.time()
+    print("In SOLVER, method", method)
+    visited_nodes = [list(starting_state)]
+    #__init__(self, state, parent, action, depth, cost, total_path_cost)
+    queue = [Node(starting_state, None, None, 0, 0, 0)]
+    nodes_expanded = 0
+    max_queue = 0
+    max_depth = 0
+    while(len(queue)>0):
+        #Update queue information
+        max_queue = max(len(queue), max_queue)
+        current_node = queue.pop()
+        
+        # print(current_node.state)
+        # print(current_node.action)
+        if current_node.state == goal_state:
+            end_time =  time.time()
+            path = []
+            curr = current_node
+            while curr.parent is not None:
+                path.insert(0, curr.action)
+                curr = curr.parent
+            #max_ram_usage = resource.RUSAGE_SELF(2)
+            print_statistics(current_node,
+                    start_time, end_time,           #time
+                    path,                           #path_to_goal
+                    current_node.total_path_cost,   #cost_of_path
+                    nodes_expanded,                 #nodes_expanded
+                    current_node.depth,             #search_depth
+                    max_depth,                      #max_search_depth
+                    max_ram_usage = None            #max_ram_usage
+                    )
+            return "GOAL STATE FOUND"
+        next_states = current_node.generate_children()
+        nodes_expanded +=1
+        for node in next_states:
+            if node.state not in visited_nodes:
+                max_depth = max(node.depth, max_depth)
+                visited_nodes.append(node.state)
+                if method == "bfs":
+                    queue.insert(0, node )
+                    #Adding to the front of a queue gives FIFO Behavior
+                else:
+                    queue.append(node)
+                    #Adding to the back of a queue gives LIFO Behavior
+        if method == "ast":
+            print(queue)
+            queue.sort(key = lambda node: node.ManhattanDistance() + node.total_path_cost, reverse = True)
+            print(queue)
+    return False
+ 
+def main():
+    method = "dfs" #sys.argv[0]
+    start_state =   1,2,5,3,4,0,6,7,8
+ #sys.argv[1]
+    #Define goal state
 
+    # __init__(self, state, parent, action, depth, cost, total_path_cost
+    board = solver(method, start_state, goal_state)
+    print(board)
+main()
