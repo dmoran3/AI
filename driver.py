9a70798f475e5356fc670b9cc734873ebe812764
dmoran3 <49569041+dmoran3@users.noreply.github.com>
2022-03-01 21:13:25 -0500

    Add files via upload

--- driver.py
+++ driver.py
@@ -1,23 +1,21 @@
+# 
+# 
+#Dan Moran
+#AI HW1 Search
+#Spring 2022
+# 
 #
-#
-#
-# 
-# 
-# 
-# 
+#-*- coding: utf-8 -*-
 
-#import numpy as np
 import sys
 import time
-#from resource import *
+from resource import *
 
 #define the edges
 top_row = [0,1,2]
 bottom_row = [6,7,8]
 right_side = [2,5,8]
 left_side = [0,3,6]
-
-goal_state = [0,1,2,3,4,5,6,7,8]
 
 class Node:
     #CONSTRUCTOR FUNCTION
@@ -37,8 +35,7 @@ class Node:
         #find the 0 in the current state
         zero = self.state.index(0)
 
-        ##Only generate children nodes for states that are reachable
-        ##Geneterate using [‘Up’, ‘Down’, ‘Left’, ‘Right’]  order given
+        #Only generate children nodes for states that are reachable
 
         #UP
         if(zero not in top_row):
@@ -112,7 +109,6 @@ def print_statistics(self, start_time, end_time, path_to_goal, cost_of_path, nod
 
 def solver(method, starting_state, goal_state):
     start_time = time.time()
-    print("In SOLVER, method", method)
     visited_nodes = [list(starting_state)]
     #__init__(self, state, parent, action, depth, cost, total_path_cost)
     queue = [Node(starting_state, None, None, 0, 0, 0)]
@@ -124,8 +120,6 @@ def solver(method, starting_state, goal_state):
         max_queue = max(len(queue), max_queue)
         current_node = queue.pop()
         
-        # print(current_node.state)
-        # print(current_node.action)
         if current_node.state == goal_state:
             end_time =  time.time()
             path = []
@@ -133,7 +127,7 @@ def solver(method, starting_state, goal_state):
             while curr.parent is not None:
                 path.insert(0, curr.action)
                 curr = curr.parent
-            #max_ram_usage = resource.RUSAGE_SELF(2)
+            max_ram_usage = getrusage(RUSAGE_SELF).ru_maxrss
             print_statistics(current_node,
                     start_time, end_time,           #time
                     path,                           #path_to_goal
@@ -141,34 +135,37 @@ def solver(method, starting_state, goal_state):
                     nodes_expanded,                 #nodes_expanded
                     current_node.depth,             #search_depth
                     max_depth,                      #max_search_depth
-                    max_ram_usage = None            #max_ram_usage
+                    max_ram_usage            #max_ram_usage
                     )
             return "GOAL STATE FOUND"
         next_states = current_node.generate_children()
         nodes_expanded +=1
+
+        temp_next = []
+        if(method != "bfs"):
+            next_states.reverse()
         for node in next_states:
             if node.state not in visited_nodes:
                 max_depth = max(node.depth, max_depth)
                 visited_nodes.append(node.state)
                 if method == "bfs":
+
                     queue.insert(0, node )
                     #Adding to the front of a queue gives FIFO Behavior
                 else:
                     queue.append(node)
                     #Adding to the back of a queue gives LIFO Behavior
         if method == "ast":
-            print(queue)
             queue.sort(key = lambda node: node.ManhattanDistance() + node.total_path_cost, reverse = True)
-            print(queue)
     return False
  
 def main():
-    method = "dfs" #sys.argv[0]
-    start_state =   1,2,5,3,4,0,6,7,8
- #sys.argv[1]
-    #Define goal state
-
+    method = sys.argv[1]
+    start_state_arg = sys.argv[2].split(',')
+    start_state = []
+    for num in range(0,len(start_state_arg)):
+        start_state.append(int(start_state_arg[num]))
+    goal_state = [0,1,2,3,4,5,6,7,8]
     # __init__(self, state, parent, action, depth, cost, total_path_cost
     board = solver(method, start_state, goal_state)
-    print(board)
 main()
